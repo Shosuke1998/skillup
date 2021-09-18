a1c3127bd61ba5496badbd4480fb9d1fb29d6008
Shosuke1998 <shosuke123@outlook.jp>
2021-09-18 20:25:33 +0900

    答えを出現させる

--- app/javascript/answer.js
+++
@@ -1,7 +0,0 @@
-function answer (){
-  const answerBtn = document.getElementById("answer-btn");
-  const answer = document.getElementById("answer");
-  
-};
-
-window.addEventListener('load', answer);
