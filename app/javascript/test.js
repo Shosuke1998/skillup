07aaa34eb073561b7438e604f3b1d99dbaef0f25
Shosuke1998 <shosuke123@outlook.jp>
2021-09-21 12:49:28 +0900

    問題作成完了

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -6,25 +6,27 @@ window.addEventListener('load', function(){
   const form = document.getElementById("form");
   const scoreArea = document.getElementById("score-area");
   const scoreInput = document.getElementById("score-input")
+  const preScore = document.getElementById("pre-score")
+  const scoreBtn = document.getElementsByClassName("score-btn")
   const testSet = [
-    {q:'byproduct',c:['副産物','b','c','d']},
-    {q:'imprudent',c:['軽率な','bf','g','dh']},
-    {q:'multipurpose',c:['多目的な','bs','cs','ds']},
-    {q:'nonprofit',c:['非営利の','ssb','ssc','dss']},
-    {q:'precede',c:['先行する,優先する','ssb','ssc','dss']},
-    {q:'precedent',c:['前例','ssb','ssc','dss']},
-    {q:'prefect',c:['知事','ssb','ssc','dss']},
-    {q:'prefectural',c:['県の','ssb','ssc','dss']},
-    {q:'preferable',c:['好ましい、望ましい','ssb','ssc','dss']},
-    {q:'prefix',c:['接頭辞','ssb','ssc','dss']},
-    {q:'pregnancy',c:['妊娠','ssb','ssc','dss']},
-    {q:'premise',c:['前提','ssb','ssc','dss']},
-    {q:'pretend',c:['ふりをする','ssb','ssc','dss']},
-    {q:'prevail',c:['普及する','ssb','ssc','dss']},
-    {q:'prevalent',c:['普及している、一般的な','ssb','ssc','dss']},
-    {q:'previous',c:['以前の、前に','ssb','ssc','dss']},
-    {q:'proceed',c:['前進する、進行する','ssb','ssc','dss']},
-    {q:'process',c:['手順、プロセス','ssb','ssc','dss']},
+    {q:'byproduct',c:['副産物','主産物','製品','不良品']},
+    {q:'imprudent',c:['軽率な','自身のある','自覚のある','無意識な']},
+    {q:'multipurpose',c:['多目的な','たくさんの','~できない','神経質な']},
+    {q:'nonprofit',c:['非営利の','営利の','問題な','軽率な']},
+    {q:'precede',c:['先行する,優先する','走る','転ぶ','倒れる']},
+    {q:'precedent',c:['前例','実体験','研究','成果']},
+    {q:'prefect',c:['知事','政治家','秘書','大臣']},
+    {q:'prefectural',c:['県の','市の','役所の','町の']},
+    {q:'preferable',c:['好ましい、望ましい','悍しい','しんどい','頑張っている']},
+    {q:'prefix',c:['接頭辞','接尾辞','真ん中の','名詞']},
+    {q:'pregnancy',c:['妊娠','誕生','診察','前提']},
+    {q:'premise',c:['前提','考察','事例','正論']},
+    {q:'pretend',c:['ふりをする','過ちを犯す','叩く','触る']},
+    {q:'prevail',c:['普及する','ふりをする','先行する','支持する']},
+    {q:'prevalent',c:['普及している、一般的な','県の','以前の','営利の']},
+    {q:'previous',c:['以前の、前に','普及している','望ましい','妊娠した']},
+    {q:'proceed',c:['前進する、進行する','ふりをする','前に手を置く','交代する']},
+    {q:'process',c:['手順、プロセス','研究','事件','犯罪']},
   ];
   let count = 0;
   let isAnswered;
@@ -93,5 +95,12 @@ window.addEventListener('load', function(){
     }
     
   });
+
+  scoreBtn.addEventListener('click',() => {
+    if(preScore === nil){
+      alert("あ");
+    }
+  })
+
 
 })
