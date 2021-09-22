56a36786382c9c5e42a588a0ae1a1f0e5dd7d046
Shosuke1998 <shosuke123@outlook.jp>
2021-09-22 10:25:13 +0900

    カウントアップ機能

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -7,30 +7,35 @@ window.addEventListener('load', function(){
   const scoreArea = document.getElementById("score-area");
   const scoreInput = document.getElementById("score-input")
   const preScore = document.getElementById("pre-score")
-  const scoreBtn = document.getElementsByClassName("score-btn")
+  const scoreBtn = document.getElementsByClassName("score-btn");
+  const timer = document.getElementById("timer");
+  const timeRecord = document.getElementById("time-record");
   const testSet = [
     {q:'byproduct',c:['副産物','主産物','製品','不良品']},
     {q:'imprudent',c:['軽率な','自身のある','自覚のある','無意識な']},
     {q:'multipurpose',c:['多目的な','たくさんの','~できない','神経質な']},
-    {q:'nonprofit',c:['非営利の','営利の','問題な','軽率な']},
-    {q:'precede',c:['先行する,優先する','走る','転ぶ','倒れる']},
-    {q:'precedent',c:['前例','実体験','研究','成果']},
-    {q:'prefect',c:['知事','政治家','秘書','大臣']},
-    {q:'prefectural',c:['県の','市の','役所の','町の']},
-    {q:'preferable',c:['好ましい、望ましい','悍しい','しんどい','頑張っている']},
-    {q:'prefix',c:['接頭辞','接尾辞','真ん中の','名詞']},
-    {q:'pregnancy',c:['妊娠','誕生','診察','前提']},
-    {q:'premise',c:['前提','考察','事例','正論']},
-    {q:'pretend',c:['ふりをする','過ちを犯す','叩く','触る']},
-    {q:'prevail',c:['普及する','ふりをする','先行する','支持する']},
-    {q:'prevalent',c:['普及している、一般的な','県の','以前の','営利の']},
-    {q:'previous',c:['以前の、前に','普及している','望ましい','妊娠した']},
-    {q:'proceed',c:['前進する、進行する','ふりをする','前に手を置く','交代する']},
-    {q:'process',c:['手順、プロセス','研究','事件','犯罪']},
+    // {q:'nonprofit',c:['非営利の','営利の','問題な','軽率な']},
+    // {q:'precede',c:['先行する,優先する','走る','転ぶ','倒れる']},
+    // {q:'precedent',c:['前例','実体験','研究','成果']},
+    // {q:'prefect',c:['知事','政治家','秘書','大臣']},
+    // {q:'prefectural',c:['県の','市の','役所の','町の']},
+    // {q:'preferable',c:['好ましい、望ましい','悍しい','しんどい','頑張っている']},
+    // {q:'prefix',c:['接頭辞','接尾辞','真ん中の','名詞']},
+    // {q:'pregnancy',c:['妊娠','誕生','診察','前提']},
+    // {q:'premise',c:['前提','考察','事例','正論']},
+    // {q:'pretend',c:['ふりをする','過ちを犯す','叩く','触る']},
+    // {q:'prevail',c:['普及する','ふりをする','先行する','支持する']},
+    // {q:'prevalent',c:['普及している、一般的な','県の','以前の','営利の']},
+    // {q:'previous',c:['以前の、前に','普及している','望ましい','妊娠した']},
+    // {q:'proceed',c:['前進する、進行する','ふりをする','前に手を置く','交代する']},
+    // {q:'process',c:['手順、プロセス','研究','事件','犯罪']},
   ];
   let count = 0;
   let isAnswered;
   let score = 0;
+  let startTime;
+  let timeoutId;
+  startTime = Date.now();
 
   function shuffle(array) {
     for(let i = array.length - 1;i > 0;i--){
@@ -48,22 +53,37 @@ window.addEventListener('load', function(){
     li.classList.add("selected")
     if(li.textContent === testSet[count].c[0]){
       score++;
-      console.log("a");
+      // console.log("a");
     } else{
-      console.log("s");
+      // console.log("s");
     }
   }
 
   
+  function countUp() {
+    console.log(Date.now() - startTime)
+    const d = new Date(Date.now() - startTime);
+    const minutes = String(d.getMinutes()).padStart(2,'0');
+    const seconds = String(d.getSeconds()).padStart(2,'0');
+    const milliseconds = String(d.getMilliseconds()).padStart(2,'0');
+    timer.innerHTML = `${minutes}:${seconds}:${milliseconds}`;
+    timeoutId = setTimeout(() => {
+      countUp();
+    },10);
+  }
+  countUp();
+    
+  
+  
+  
+  
   function setTest(){
-    // timer();
     isAnswered = false;
     testNum.innerHTML = count + 1;
     question.innerHTML = testSet[count].q;
     while(choices.firstChild){
       choices.removeChild(choices.firstChild)
     }
-    console.log(testSet[count].c)
     const shuffledChoices = shuffle([...testSet[count].c])
     shuffledChoices.forEach((choice) => {
       const li = document.createElement("li");
@@ -82,12 +102,23 @@ window.addEventListener('load', function(){
   }
   setTest();
 
+  
+
+
   btn.addEventListener('click',() => {
     if(isAnswered === false){
       return
     }
     if(count === testSet.length - 1){
       scoreArea.innerHTML = `Score:${score} / ${testSet.length}`
+      clearTimeout(timeoutId);
+      console.log(timeoutId)
+      console.log(Date.now() - startTime)
+      const d = new Date(Date.now() - startTime);
+      const m = String(d.getMinutes()).padStart(2,'0');
+      const s = String(d.getSeconds()).padStart(2,'0');
+      const ms = String(d.getMilliseconds()).padStart(2,'0');
+      timeRecord.innerHTML = `${m}:${s}:${ms}`;
       // scoreInput.innerHTML = score;
       form.classList.remove("hidden");
     } else {
@@ -97,15 +128,7 @@ window.addEventListener('load', function(){
     
   });
 
-  function timer(){
-    let cnt = 5;
-    console.log(cnt)
-    const timeoutId = setTimeout(timer,1000);
-    cnt--;
-    if(cnt === 0){
-      clearTimeout(timeoutId);
-    }
-  }
+  
   
 
 
