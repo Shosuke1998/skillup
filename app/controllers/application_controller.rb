e5a87e9b9c3356628542fa400ac0f2ddf2afb3c3
Shosuke1998 <shosuke123@outlook.jp>
2021-09-26 19:43:04 +0900

    ストロングパラメータ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys: [:name, :pre_score, :input_words])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:name, :pre_score, :input_words, :preposition_score])
   end
 end
